7241d4af0ea01a9f0ab32250560666c21f7aa205
jdgilhuly <jdgilhuly@icloud.com>
2022-02-21 18:36:11 -0800

    Layouts and some pages

---
+++ frontend-react/src/pages/Listings.js
@@ -0,0 +1,36 @@
+//rafce
+import React from 'react'
+import {Card, Button, ListGroup, Row, Col} from "react-bootstrap"
+
+//can pass listing info into this function later
+const Listings = (numberOfListings) => {
+  return (
+    <div>
+      {/* md is number of cards per row 
+      This iteartes through an array of 4 and creates cards for em*/}
+      <Row xs={1} md={2} className="g-4 mt-2 mx-2">
+        {Array.from({ length: 4 }).map((_, idx) => (
+          <Col>
+            <Card>
+              <Card.Img variant="top" src="holder.js/100px160" />
+              <Card.Body>
+                <Card.Title>Card title</Card.Title>
+                <Card.Text>
+                  Some quick example text to describe the building would go right here. 
+                </Card.Text>
+                <ListGroup className= "text-center" variant="flush">
+                  <ListGroup.Item>Info Category 1</ListGroup.Item>
+                  <ListGroup.Item>Info Category 2</ListGroup.Item>
+                  <ListGroup.Item>Info Category 3</ListGroup.Item>
+                </ListGroup>
+                <Button className= "mx-2" variant = "primary">Bid Now</Button>
+              </Card.Body>
+            </Card>
+          </Col>
+        ))}
+        </Row>
+    </div>
+  )
+}
+
+export default Listings
