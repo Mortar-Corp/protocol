934a173ac88bdd41b675a9de8bc8f71305926ef8
jdgilhuly <jdgilhuly@icloud.com>
2022-02-21 18:35:39 -0800

    Navbar and Cards  init + some routing

--- frontend-react/src/App.js
+++ frontend-react/src/App.js
@@ -1,18 +1,23 @@
 import React from 'react';
 
-import './App.css';
-import Navbar from './components/Navbar';
+import Header from './components/Header';
+import Listings from './pages/Listings';
+import Home from './pages/Home';
+import { Switch} from "react-router-dom";
 
-import { BrowserRouter, Routes, Route} from "react-router-dom";
 
+import {BrowserRouter, Routes, Route} from "react-router-dom";
 
 function App() {
-  const name = "yeet"
   return (
     <div className = "App">
-      <Navbar />
-      <h1>hey {name}</h1>
-      
+      <Header />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/home" exact component ={<Home />}/>
+          <Route path="/listings" exact component ={<Listings />}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- frontend-react/src/components/Navbar.js
+++
@@ -1,9 +0,0 @@
-const Navbar = () => {
-  return (
-    <header>
-        <h1>Test Navbar</h1>
-    </header>
-  )
-}
-
-export default Navbar
